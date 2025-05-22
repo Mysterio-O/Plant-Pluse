e45449fb83311db8b305b38f378df12e29ce0caa
Mysterio-O <skrabbi.019@gmail.com>
2025-05-22 20:27:28 +0600

    added lottie animation on edit plant page

--- src/components/EditPlant/EditPlant.jsx
+++ src/components/EditPlant/EditPlant.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import EditPlantForm from '../../pages/EditPlantForm';
 import { AuthContext } from '../../Provider/AuthProvider';
 import Loader from "../../pages/Loader"
+import Lottie from '../Lottie/Lottie';
 const EditPlant = () => {
 
     const { user } = useContext(AuthContext);
@@ -36,22 +37,27 @@ const EditPlant = () => {
             <div className='md:max-w-2xl mx-auto space-y-3 flex flex-col justify-center items-center'>
                 <h2 className="text-5xl text-center text-[#FAFAFA] rancho-regular">Welcome {displayName}</h2>
                 <span>{email}</span>
+                <div className='mask mask-squircle h-24 w-24'>
+                    <img src={photoURL} alt={`${displayName}'s photo`} className='' />
+                </div>
+                
                 <p className='text-center text-[#FAFAFA] text-xl'>your plant’s information to ensure it stays healthy and properly categorized. You can change its name, category, care level, and image. Don’t forget to save your changes!</p>
 
                 <button
                     onClick={() => setIsOpen(!isOpen)}
                     className='btn rounded-3xl px-4 py-1 bg-[#FFCA28] hover:bg-transparent hover:text-[#FFCA28]'>
-                        {
-                            !isOpen ? 'Update Plant' : 'Hide Form'
-                        }
-                    </button>
+                    {
+                        !isOpen ? 'Update Plant' : 'Hide Form'
+                    }
+                </button>
+                <Lottie/>
             </div>
 
             {
                 loading ? <Loader />
                     : <div className={`backdrop-blur-sm bg-white/10 rounded-2xl p-10 shadow-2xl w-full max-w-4xl mt-10 mx-auto ${!isOpen ? 'opacity-0' : 'opacity-100'} transition-all duration-500`}>
 
-                        <EditPlantForm plant={plant} isOpen={isOpen}/>
+                        <EditPlantForm plant={plant} isOpen={isOpen} />
                     </div>
             }
 
---
+++ src/components/Lottie/Lottie.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import PlantGrow from '../../assets/lotties/lottie-animation.json'
+import { useLottie, useLottieInteractivity } from 'lottie-react';
+
+const style = {
+    height: 200    
+    }
+    const options = {
+        animationData: PlantGrow,
+        autoplay: true
+    }
+const Lottie = () => {
+    
+    const lottieObj = useLottie(options, style);
+    const Animation = useLottieInteractivity({
+        lottieObj,
+        mode:'scroll',
+        actions: [{
+            visibility:[0.2, 1],
+            type:'seek',
+            frames:[0, 120]
+        }]
+    })
+    return Animation
+};
+
+export default Lottie;
